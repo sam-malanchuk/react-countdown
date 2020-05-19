2470b62da95afc5024749fa78456cd2426b5ce73
Sam Malanchuk <malanchuksam@gmail.com>
2020-05-18 21:33:12 -0400

    created the counter reset button and method for it

--- my-app/src/Counter.js
+++ my-app/src/Counter.js
@@ -5,22 +5,33 @@ class Counter extends React.Component {
         super(props);
 
         this.state = {
+            setCounter: 0,
             counter: 4,
         }
 
+        // minus one every time the counter is clicked
         this.updateCounter = () => {
             if(this.state.counter > 1) {
                 this.setState({counter: this.state.counter - 1})
+            // if the counter is already at zero then keep it there
             } else {
                 this.setState({counter: 0})
             }
-        }
+        };
+
+        // Set the counter back to the setCounter value
+        this.resetCounter = () => this.setState({counter: this.state.setCounter});
     }
 
     render() {
         return (
-            <div className="counterBox" onClick={this.updateCounter}>
-                {this.state.counter}
+            <div>
+                <div className="counterBox" onClick={this.updateCounter}>
+                    {this.state.counter}
+                </div>
+                <div>
+                    <button onClick={this.resetCounter}>Reset Counter</button>
+                </div>
             </div>
         )
     }
