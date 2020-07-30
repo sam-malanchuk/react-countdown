816f44d563569b82ac00653d4269f4d682532a1c
Sam Malanchuk <malanchuksam@gmail.com>
2020-07-30 18:09:08 -0400

    added menu component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Counter from './Counter.js';
+import Menu from './Menu.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu />
         <Counter />
       </header>
     </div>
---
+++ my-app/src/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Menu() {
+    return (
+        <div>
+            <p>Welcome to the React Click Countdown</p>
+            <button>Start</button>
+        </div>
+    )
+}
+
+export default Menu;
